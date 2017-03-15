12316abfcba012a589d8ee9b148eeffd9d979903
df0o <mo.susie.yu@gmail.com>
2017-03-15 01:15:11 -0400

    added jquery smooth transitions and about content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-function init() {
-    window.addEventListener('scroll', function(e) {
+$(document).ready(function() {
+    $("#title").css("opacity", 0);
+    $("#title").animate({ "top": "-=50px" }, { queue: false, duration: 1000 });
+    $("#title").animate({ opacity: 1 }, { queue: false, duration: 3500 });
+    $(".navop").css("opacity", 0);
+    $(".navop").animate({ opacity: 1, "top": "+=50px" }, { queue: false, duration: 1500 });
+    $("#title-sub").css("opacity", 0);
+    $("#title-sub").animate({ "top": "-=50px" }, { queue: false, duration: 1200 });
+    $("#title-sub").animate({ opacity: 1 }, { queue: false, duration: 4000 });
+    $("#scrolldown").css("opacity", 0);
+    $("#scrolldown").animate({ opacity: 1 }, { queue: false, duration: 4000 });
+
+    $(window).on("scroll", function() {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 200,
             nav = document.querySelector("nav");
@@ -10,6 +21,13 @@ function init() {
                 classie.remove(nav, "smaller");
             }
         }
-    });
-}
-window.onload = init();
+    })
+});
+
+$(document).on("click", ".navop", function(event) {
+    event.preventDefault();
+    var target = this.getAttribute("href");
+    $("html, body").animate({
+        scrollTop: $(target).offset().top
+    }, 2000);
+});
