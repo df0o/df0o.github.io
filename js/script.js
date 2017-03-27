31256a54efe430057f9b61243d9158fdcd221ca1
df0o <mo.susie.yu@gmail.com>
2017-03-27 01:51:51 -0400

    affed fade

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     $("#title").css("opacity", 0);
-    $("#title").animate({ "top": "-=50px" }, { queue: false, duration: 1000 });
-    $("#title").animate({ opacity: 1 }, { queue: false, duration: 3500 });
+    $("#title").animate({ "top": "-=50px" }, { queue: false, duration: 1500 });
+    $("#title").animate({ opacity: 1 }, { queue: false, duration: 2500 });
     $(".navop").css("opacity", 0);
-    $(".navop").animate({ opacity: 1, "top": "+=50px" }, { queue: false, duration: 1500 });
+    $(".navop").animate({ opacity: 1, "top": "+=50px" }, { queue: false, duration: 1200 });
     $("#title-sub").css("opacity", 0);
     $("#title-sub").animate({ "top": "-=50px" }, { queue: false, duration: 1200 });
-    $("#title-sub").animate({ opacity: 1 }, { queue: false, duration: 4000 });
+    $("#title-sub").animate({ opacity: 1 }, { queue: false, duration: 3000 });
     $("#scrolldown").css("opacity", 0);
     $("#scrolldown").animate({ opacity: 1 }, { queue: false, duration: 4000 });
 
@@ -30,4 +30,19 @@ $(document).on("click", ".navop", function(event) {
     $("html, body").animate({
         scrollTop: $(target).offset().top
     }, 2000);
+});
+
+
+$(window).scroll(function() {
+    $('.fadeInBlock').each(function(i) {
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        bottom_of_window = bottom_of_window + 80;
+
+        if (bottom_of_window > bottom_of_object) {
+
+            $(this).animate({ 'opacity': '1' }, 1000);
+        }
+    });
 });
